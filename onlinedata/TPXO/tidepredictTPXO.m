65855ab583a788d3953c5bf716205ff8a1e8f8ce
olavobm <olavobm@gmail.com>
2017-01-12 17:24:10 -0800

    intermediary commit. Function starting to look good.

--- onlinedata/TPXO/tidepredictTPXO.m
+++ onlinedata/TPXO/tidepredictTPXO.m
@@ -2,15 +2,66 @@ function tidepred = tidepredictTPXO(time, lon, lat, tidevar, tpxomodel)
 % tidepred = TIDEPREDICTTPXO(time, lon, lat, tidevar, tpxomodel)
 %
 %   inputs:
-%       - time:
-%       - lon:
-%       - lat:
+%       - time: time in datenum format to (UTC????)
+%       - lon: longitude.
+%       - lat: latitude.
 %       - tidevar:
-%       - tpxomodel:
+%       - tpxomodel (optional):
 %
 %   outputs:
-%       - tidepred
+%       - tidepred:
 %
-% TIDEPREDICTTPXO uses the tmd_toolbox to make prediction
+% TIDEPREDICTTPXO uses the tmd_toolbox to make a tidal prediction
+% using the TPXO model.
 %
 % Olavo Badaro Marques, 12/Jan/2017.
+
+
+%% If the optional input is not given,
+% then use a default TPXO model:
+
+
+if ~exist('tpxomodel', 'var')
+    tpxomodeldir = ['/Users/olavobm/Documents/MATLAB/olavo_toolbox/' ...
+                    'others/tmd_toolbox/DATA/'];
+	tpxomodelfile = 'Model_tpxo7.2';
+    
+    tpxomodel = fullfile(tpxomodeldir, tpxomodelfile);
+end
+
+
+%
+if exist(tpxomodel, 'file') ~=2
+    error(['TPXO*.* file ' tpxomodel ' does not exist'])
+end
+
+%%
+
+if ~exist('tidevar', 'var')
+    
+%     tidevar = {'u', 'v', 'z'};
+    tidevar = 'z';
+
+end
+
+%%
+time = time(:);
+
+if isrow(lon)
+    lon = lon(:);
+end
+
+if isrow(lat)
+    lat = lat(:);
+end
+
+
+%% Uses evalc so that things are not printed on the screen:
+
+strTimeCoords = 'time, lat, lon';
+strVar = tidevar;
+
+strRun = ['tidepred = tmd_tide_pred(''' tpxomodel ''', ' strTimeCoords ', ''' strVar ''');'];
+evalc(strRun);
+
+tidepred = tmd_tide_pred(tpxomodel, time, lat, lon, tidevar);
