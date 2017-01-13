f8f364fe8badaf1a89902c3215fbc9f1b0ca8154
olavobm <olavobm@gmail.com>
2017-01-12 16:24:29 -0800

    initial commit.

---
+++ onlinedata/TPXO/tidepredictTPXO.m
@@ -0,0 +1,16 @@
+function tidepred = tidepredictTPXO(time, lon, lat, tidevar, tpxomodel)
+% tidepred = TIDEPREDICTTPXO(time, lon, lat, tidevar, tpxomodel)
+%
+%   inputs:
+%       - time:
+%       - lon:
+%       - lat:
+%       - tidevar:
+%       - tpxomodel:
+%
+%   outputs:
+%       - tidepred
+%
+% TIDEPREDICTTPXO uses the tmd_toolbox to make prediction
+%
+% Olavo Badaro Marques, 12/Jan/2017.
