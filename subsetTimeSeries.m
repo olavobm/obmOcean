86115242c34f3519ded3a8e8a8697399f384be5d
olavobm <olavobm@gmail.com>
2017-03-13 11:11:18 -0700

    initial commit.

---
+++ subsetTimeSeries.m
@@ -0,0 +1,17 @@
+function [tout, xout] = subsetTimeSeries(dlims, t, x, varfields, tavgstep)
+% [tout, xout] = SUBSETTIMESERIES(dlims, t, x, varfields, tavgstep)
+%
+%   inputs:
+%       - dlims
+%       - t:
+%       - x:
+%       - varfields:
+%       - tavgstep:
+%
+%   outputs:
+%       - tout:
+%       - xout:
+%
+%
+%
+% Olavo Badaro Marques, 13/Mar/2017.
