d5e41d88621a94d3b3ab1c1f31f3d2221aa87ebd
olavobm <olavobm@gmail.com>
2017-04-01 22:20:10 -0700

    looking good but need some improvements.

--- subsetProfilerTimeseries.m
+++ subsetProfilerTimeseries.m
@@ -21,82 +21,53 @@ function [xout] = subsetProfilerTimeseries(x, varfields, d, dlims, t, tavgstep)
 %%
 
 
+if ischar(d)
+    d = {d};
+end
 
-indcell = 
-
-
+indvarcell = d;
 
 indvarlims = dlims;
 
-varcell = 
-
-structout = subsetStruct(indvarcell, indvarlims, structvar, varcell);
-
+%
+xaux = subsetStruct(indvarcell, indvarlims, x, varfields);
 
 %%
 
-% For simplicity, subset the data for when McLane profiled correctly:
-% ydaysubset = [25 60];
-ydaysubset = [15 60];
-MPsub = MP109.aqdp;
-linsubset = MPsub.yday>=ydaysubset(1) & MPsub.yday<=ydaysubset(2);
-MPsub.yday = MPsub.yday(linsubset);
-MPsub.p = MPsub.p(linsubset);
-MPsub.u = MPsub.u(linsubset);
-MPsub.v = MPsub.v(linsubset);
-MPsub.w = MPsub.w(linsubset);
-MPsub.pitch = MPsub.pitch(linsubset);
-MPsub.roll = MPsub.roll(linsubset);
-
-MPnearbot = MPsub;
-lnearbot = MPnearbot.p>=1643 & MPnearbot.p<=1653;
-MPnearbot.yday = MPnearbot.yday(lnearbot);
-MPnearbot.u = MPnearbot.u(lnearbot);
-MPnearbot.v = MPnearbot.v(lnearbot);
-MPnearbot.w = MPnearbot.w(lnearbot);
-MPnearbot.p = MPnearbot.p(lnearbot);
-MPnearbot.pitch = MPnearbot.pitch(lnearbot);
-MPnearbot.roll = MPnearbot.roll(lnearbot);
-
-    
-% Take the time difference. When it goes to another profile,
-% there is a difference of 15 to 40 minutes:
-lsameprof = (diff(MPnearbot.yday)*24*60) < 1;
+% Take the time difference and identify different profiles:
+lsameprof = diff(xaux.(t)) < tavgstep;
 
 inddiffprof = find(~lsameprof);
 ndiffprof = length(inddiffprof);
 
-indall = length(MPnearbot.yday);
 
 % Pre-allocate:
-MPbotavg.yday = NaN(1, ndiffprof);
-MPbotavg.u = NaN(1, ndiffprof);
-MPbotavg.v = NaN(1, ndiffprof);
-MPbotavg.w = NaN(1, ndiffprof);
-MPbotavg.p = NaN(1, ndiffprof);
-MPbotavg.pitch = NaN(1, ndiffprof);
-MPbotavg.roll = NaN(1, ndiffprof);
+avgfields = [d, varfields];
+xout = xaux;
+
+for i = 1:length(avgfields)
+    xout.(avgfields{i}) = NaN(ndiffprof, 1);
+end
 
 %
-avgfields = fieldnames(MPbotavg);
-
 indbeg = 1;
 
-% Loop through profiles and take the average:
+% Loop over profiles and take the average:
 for i1 = 1:ndiffprof
-    
     
     indend = inddiffprof(i1);
     
     indavg = indbeg : indend;
     
     for i2 = 1:length(avgfields)
-        MPbotavg.(avgfields{i2})(i1) = mean(MPnearbot.(avgfields{i2})(indavg));
+            
+            xout.(avgfields{i2})(i1) = mean(xaux.(avgfields{i2})(indavg));
+        
     end
    
-    if (MPnearbot.yday(indend)-MPnearbot.yday(indbeg)*24*60)>1
-        warning('!!!')
-    end
+%     if (xaux.(t)(indend)-xaux.(t)(indbeg)*24*60)>1
+%         warning('!!!')
+%     end
     
     % Update indice:
     indbeg = indend + 1;
