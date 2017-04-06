94ce885e6b3ca172bbe3030bc5df6340f03fdfdd
olavobm <olavobm@gmail.com>
2017-04-05 22:10:33 -0700

    initial commit.

---
+++ breakintoProfiles.m
@@ -0,0 +1,15 @@
+function [indprof, tpcell] = breakintoProfiles(t, p, dtprof, plims)
+%
+%
+%   inputs:
+%       - t:
+%       - p:
+%       - dtprof:
+%       - plims:
+%
+%   outputs:
+%       - indprof:
+%       - tpcell:
+%
+%
+% Olavo Badaro Marques, 05/Apr/2017.
