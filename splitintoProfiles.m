cf6742939ebc861e6db4a3d7e602ae7c25873c69
olavobm <olavobm@gmail.com>
2017-04-05 22:25:29 -0700

    changed function name.

---
+++ splitintoProfiles.m
@@ -0,0 +1,27 @@
+function [indprof, tpcell] = splitintoProfiles(dt, t, p, plims)
+% [indprof, tpcell] = SPLITINTOPROFILES(t, p, dt, plims)
+%
+%   inputs:
+%       - dt:
+%       - t:
+%       - p:
+%       - plims (optional):
+%
+%   outputs:
+%       - indprof:
+%       - tpcell:
+%
+%
+% Olavo Badaro Marques, 05/Apr/2017.
+
+
+%%
+
+% % Do this later:
+% if exist('plims', 'var')    
+%     linplims = (p>=plims(1) & p<=plims(2));
+% end
+
+
+%%
+
