914e59e67f57a6e1781e6662b6665517bde36da9
olavobm <olavobm@gmail.com>
2017-04-07 16:28:39 -0700

    major update. Translated two hard-coded parameters to input and included default values for them.

--- wfromdpdt.m
+++ wfromdpdt.m
@@ -1,9 +1,11 @@
-function wout  = wfrompt(t, p)
-% [tout, w] = WFROMPT(t, p)
+function wout  = wfromdpdt(t, p, ntunits, tskip)
+% [tout, w] = WFROMDPDT(t, p, tskip)
 %
 %   inputs:
 %       - t: time.
 %       - p: pressure.
+%       - ntunits (optional): normalization to the units.
+%       - tskip (optional):
 %
 %   outputs:
 %       - tout: time for the vertical velocity output.
@@ -14,25 +16,39 @@ function wout  = wfrompt(t, p)
 % Olavo Badaro Marques, 20/Mar/2017.
 
 
+%%
+
+
+if ~exist('ntunits', 'var') || isempty(ntunits)
+    warning(['ntunits is not given as input. For now, make sure the ' ...
+             'script is not expecting the old version of ' ...
+             'function ' mfilename ''])
+    ntunits = 1;
+end
+
+
+%% A few useful parameters:
+
+if ~exist('tskip', 'var')
+    warning(['tskip is not given as input. For now, make sure the ' ...
+             'script is not expecting the old version of ' ...
+             'function ' mfilename ''])
+    tskip = Inf;
+end
+
+
 %% Make both inputs column vectors:
 
 t = t(:);
 p = p(:);
 
 
-%% A few useful parameters:
-
-limcontmeas = 1/(24*60/5);    % 5 minutes
-
-perday2persec = 1 / (3600*24);
-
-
-%% Compute w from 
+%% Compute w from pressure differentiation:
 
 t_diff = diff(t);
 p_diff = diff(p);
 
-lskip = (t_diff > limcontmeas);
+lskip = (t_diff > tskip);
 
 %
 dpdt = p_diff ./ t_diff;
@@ -41,14 +57,15 @@ dpdt = p_diff ./ t_diff;
 dpdt(lskip) = NaN;
 
 %
-w = dpdt * perday2persec;
+w = dpdt / ntunits;
 
 tout = (t(1:end-1) + t(2:end))/2;
 
 
-%% Filter w 
+%% Have an option to filter w (which is noisy after differentiation)
 
 
-%%
+%% Assign output:
+
 wout.time = tout;
 wout.w = w;
