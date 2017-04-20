728189a6541bc37d4b27a59e3ebd8e01c6ae8b36
olavobm <olavobm@gmail.com>
2017-04-20 15:27:28 -0700

    very simple function (though I ideally need to make some changes to be more rigorous).

--- buoyFreqfromTS.m
+++ buoyFreqfromTS.m
@@ -1,12 +1,37 @@
-function N = buoyFreqfromTS(p, t, s)
-% N = BUOYFREQFROMTS(p, t, s)
+function N2 = buoyFreqfromTS(p, t, s, zcut, ptscode)
+% N2 = BUOYFREQFROMTS(p, t, s, zcut, ptscode)
 %
 %   inputs:
-%       -
-%       -
-%       -
+%       - p:
+%       - t:
+%       - s:
+%       - zcut (optional):
+%       - ptscode (optional):
 %
 %   outputs:
-%       -
+%       - N2: structure with two fields -- (1) z, the location in the
+%             vertical and (2) N2, the buoyancy frequency squared, in
+%             (radians/s)^2.
+%
+% This function computes potential density (relative to the surface,
+% using the seawater toolbox) from the inputs and calls the function
+% buoyFreqsqrd.m to calculate the buoyancy squared from the potential
+% density.
 %
 % Olavo Badaro Marques, 20/Apr/2017.
+
+
+%% Compute potential density:
+
+% Choose reference pressure:
+refpres = 0;    % does this matter for the gradient of sgth???
+
+%
+sgth = sw_pden(s, t, p, refpres);
+
+
+%% Compute buoyancy frequency squared:
+
+N2 = buoyFreqsqrd(p, sgth, zcut, ptscode);
+
+
