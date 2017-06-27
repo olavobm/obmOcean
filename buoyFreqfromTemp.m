f0df1eeccb50e95b8cf5c5101fcef97a1d8d84d2
olavobm <olavobm@gmail.com>
2017-06-27 10:41:35 -0700

    initial commit.

---
+++ buoyFreqfromTemp.m
@@ -0,0 +1,15 @@
+function N = buoyFreqfromTemp(p, t, T0, S0)
+%
+%
+%   inputs:
+%       -
+%       -
+%       - 
+%       - S0 (optional)
+%
+%   outputs:
+%       -
+%
+%
+%
+% Olavo Badaro Marques, 20/Apr/2017.
