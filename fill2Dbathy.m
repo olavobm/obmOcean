aa9594710c6ec24fcc4db41cf71e6c8255ce4d90
olavobm <olavobm@gmail.com>
2018-06-22 18:38:08 -0700

    initial commit.

---
+++ fill2Dbathy.m
@@ -0,0 +1,18 @@
+function hfill = fill2Dbathy(x, z, zbound)
+% hfill = FILL2DBATHY(x, z, zbound)
+%
+%   inputs
+%       - x:
+%       - z:
+%       - zbound (optional):
+%
+%   outputs
+%       - hfill:
+%
+%
+%
+%
+%
+%
+%
+% Olavo Badaro Marques, 22/Jun/2018.
